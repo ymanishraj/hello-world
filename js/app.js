8d3c41928115e4c7f18c1f771f477f5ac1ef968b
Manish Raj Yadav <76259764+ymanishraj@users.noreply.github.com>
2023-09-04 14:33:04 +0000

    Add Add and subtract method

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@ const element = document.getElementById('name');
 elment.addEventListener('click', event => { alert('Hello World') } );
 
 const table = document.createElement('table');
+
+
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
